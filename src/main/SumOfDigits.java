85c7bb12261dd7847e9c5ce68e334a2443a5f338
chisanupong45 <chisanupong.p@kkumail.com>
2023-10-10 16:09:56 +0700

    Add SumOfDigits.java

---
+++ src/main/SumOfDigits.java
@@ -0,0 +1,15 @@
+package main;
+
+public class SumOfDigits {
+
+    public int sumOfDigits(int n) {
+        int sum = 0;
+        while (n > 9) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum + n;
+    }
+
+}

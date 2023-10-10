1001a32660697e75e45235bce9af5fca52f4d497
chisanupong45 <chisanupong.p@kkumail.com>
2023-10-10 20:09:03 +0700

    Update Code after test

--- src/main/SumOfDigits.java
+++ src/main/SumOfDigits.java
@@ -1,15 +1,18 @@
 package main;
 
 public class SumOfDigits {
-
+    
     public int sumOfDigits(int n) {
-        int sum = 0;
         while (n > 9) {
-            sum += n % 10;
-            n /= 10;
+            int sum = 0;
+            while (n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
         }
-
-        return sum + n;
+        
+        return n;
     }
 
 }
